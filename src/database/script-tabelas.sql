971f2c608240ebb9a10e33315c0a377089876ce2
felipzp <felipe.ferraz@sptech.school>
2025-06-16 10:59:52 -0300

    Subindo banco de dados

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,62 +1,49 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+create database FrameByFrame;
+use FrameByFrame;
+drop database FrameByFrame;
 
-/*
-comandos para mysql server
-*/
+create table empresa 
+(idEmpresa int primary key auto_increment,
+razao_social varchar(50),
+cnpj char(14),	
+codigoAcesso varchar(50));
 
-CREATE DATABASE aquatech;
+create table usuario 
+(idUsuario int primary key auto_increment,
+nome VARCHAR(50),
+email VARCHAR(50),
+senha VARCHAR(50),
+cpf char(11),
+permissao tinyint,
+fkEmpresa int,
+nivel int,
+constraint chkNivel check (nivel = 0 or 1),
+foreign key (fkEmpresa) references empresa(idEmpresa));
 
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
+CREATE TABLE alerta (
+idAlerta INT PRIMARY KEY AUTO_INCREMENT,
+titulo VARCHAR(100),
+descricao VARCHAR(150),
+fk_usuario INT,
+FOREIGN KEY (fk_usuario) REFERENCES usuario(idUsuario)
 );
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-);
+create table estudio 
+(idEstudio int primary key auto_increment,
+nome_estudio varchar(50),
+fkEmpresa int,
+foreign key (fkEmpresa) references empresa(idEmpresa));
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+create table fotometro 
+(idFotometro int primary key auto_increment,
+luminosidade int,
+temperaturaCor int,
+tipoLuz varchar(45),
+ModificadoresLuz varchar(45),
+fkEstudio int,
+foreign key (fkEstudio) references estudio(idEstudio));
 
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
 
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
+
